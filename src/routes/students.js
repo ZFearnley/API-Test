a81b6d915337f902ddabe01e54506b39dc9a2376
ZFearnley <ZFearnley213@gmail.com>
2023-10-18 10:47:47 +1300

    Added update method

--- src/routes/students.js
+++ src/routes/students.js
@@ -32,3 +32,52 @@ router.get('/:id', async (req, res, next) => {
         next(error)
     }
 })
+
+router.post('/', async (req, res, next) => {
+    try{
+        const { name, address} = req.body
+        await schema.validateAsync({name, address})
+
+        const student = await student.findOne({
+            name
+        })
+
+        if(student) {
+            const error = new Error('Student already exists')
+            res.status(409)
+            return next(error)
+        }
+
+        const newStudent = await students.insert({
+            name,
+            address
+        })
+        res.status(201).json(newStudent)
+    } catch(error){
+        next(error)
+    }
+})
+
+router.put('/:id', async (req, res, next) => {
+    try{
+        const { id } = req.params
+        const { name, address} = req.body
+        const result = await schema.validateAsync({name, address})
+        const student = await students.findOne({
+            _id:id
+        })
+
+        if(!student) {
+            return next()
+        }
+
+        const updatedStudent = await student.update({
+            _id:id,
+        }, {$set:result}, {upsert: true})
+
+        res.json(updatedStudent)
+
+    } catch(error){
+        next(error)
+    }
+})

ce39ada9e3b2f34fd577d982e8137e5c6622355f
ZFearnley <ZFearnley213@gmail.com>
2023-10-18 09:30:31 +1300

    added Students.js

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const morgan = require('morgan')
+const helmet = require('helmet')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+//const students = require('./routes/students')
+
+//app.use(notFound)
+//app.use(errorHandler)
+
+module.exports = app
---
+++ src/routes/students.js
@@ -0,0 +1,15 @@
+const Joi = require('joi')
+const schema = Joi.object({
+    name: Joi.string()
+    .min(3)
+    .max(30)
+    .required(),
+
+    address: Joi.string()
+    .min(3)
+    .max(30)
+    .required(),
+
+})
+
+module.exports = schema
